fea89882fb6e2e2e412c0d6f117e6e47e456d1f9
Ajosephp <128110702+Ajosephp@users.noreply.github.com>
2025-08-14 20:10:16 -0700

    Learning DNS package

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,32 @@
-console.log("Welcome - DNS Checker");
+import dns from 'node:dns';
+
+
+function main() {
+    console.log("Welcome - DNS Checker\n");
+
+
+
+    dns.lookup('example.com', (err: NodeJS.ErrnoException | null, address: string, family: number) => {
+        if (err) throw err;
+
+        console.log('System Resolvers:');
+        console.log('address: %j family: IPv%s', address, family);
+    });
+
+    dns.resolve4('example.com', (err: NodeJS.ErrnoException | null, addresses: string[]) => {
+        if (err) {
+            console.error('Error resolving A records:', err);
+        }
+        console.log('A records:', addresses);
+    });
+
+    dns.resolve6('example.com', (err: NodeJS.ErrnoException | null, addresses: string[]) => {
+        if (err) {
+            console.error('Error resolving AAAA records:', err);
+        }
+        console.log('AAAA records:', addresses);
+    });
+}
+
+main();
 
